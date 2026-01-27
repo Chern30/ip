523845ca95aff842e4be6e2582f21b5e7041ff3f
Chern30 <148644407+Chern30@users.noreply.github.com>
2026-01-27 17:44:03 +0800

    Level 3 done

--- src/main/java/blub.java
+++ src/main/java/blub.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 public class blub {
 
+    public record Pair(String x, String y) {}
+
     static String bot_name = "Blub";
-    static ArrayList<String> bot_brain = new java.util.ArrayList<String>();
+    static ArrayList<Pair> bot_brain = new java.util.ArrayList<Pair>();
 
     public static void main(String[] args) {
         sayHi();
-        accumulate();
+        converse();
         sayBye();
-
     }
 
     public static void sayHi() {
@@ -25,7 +26,7 @@ public class blub {
         System.out.println(bye_message);
     }
 
-    public static void echo() {
+    public static void converse() {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -33,37 +34,70 @@ public class blub {
 
             if (user_input.equals("bye")) {
                 break;
+            } else if (user_input.equals("list")) {
+                vomit();
+            } else if (user_input.matches("mark \\d+")) {
+                mark(Integer.parseInt(user_input.substring(5)) - 1);
+            } else {
+                add(user_input);
             }
 
-            System.out.println(user_input);
+            System.out.println("\n");
         }
 
         sc.close();
     }
 
-    public static void accumulate() {
-        Scanner sc = new Scanner(System.in);
+    // public static void echo() {
+    //     Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            String user_input = sc.nextLine();
+    //     while (true) {
+    //         String user_input = sc.nextLine();
 
-            if (user_input.equals("list")) {
-                vomit();
-                break;
-            }
+    //         if (user_input.equals("bye")) {
+    //             break;
+    //         }
 
-            bot_brain.add(user_input);
-            System.out.println("added: " + user_input);
-        }
+    //         System.out.println(user_input);
+    //     }
 
-        sc.close();
+    //     sc.close();
+    // }
+
+    // public static void accumulate() {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     while (true) {
+    //         String user_input = sc.nextLine();
+
+    //         if (user_input.equals("list")) {
+    //             vomit();
+    //             break;
+    //         }
+
+    //         bot_brain.add(user_input);
+    //         System.out.println("added: " + user_input);
+    //     }
+
+    //     sc.close();
+    // }
+
+    public static void add(String user_input) {
+        bot_brain.add(new Pair( " ", user_input));
+        System.out.println("added: " + user_input);
     }
 
     public static void vomit() {
-
-        System.out.println("\n")
         for (int i = 0; i < bot_brain.size(); i++) {
-            System.out.println((i + 1) + ". " + bot_brain.get(i));
+            Pair found = bot_brain.get(i);
+            System.out.println((i + 1) + ". [" + found.x() + "] " + found.y());
         }
+    }
+
+    public static void mark(int index) {
+        Pair old_record = bot_brain.get(index);
+        bot_brain.set(index, new Pair("X", old_record.y()));
+        System.out.println("Nice I have marked this task as done");
+        System.out.println("[X] " + old_record.y());
     }
 }
