7da24f38341cff6424067b626f6a559d1a1af798
Chern30 <148644407+Chern30@users.noreply.github.com>
2026-01-27 16:52:21 +0800

    initial commit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# blub User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/blub.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class blub {
+
+    static String bot_name = "Blub";
+    static ArrayList<String> bot_brain = new java.util.ArrayList<String>();
+
+    public static void main(String[] args) {
+        sayHi();
+//        echo();
+        sayBye();
+
+    }
+
+    public static void sayHi() {
+        String welcome_message =
+                "Hello! I am " + bot_name + "\n" +
+                "What can I do you for \n\n";
+        System.out.println(welcome_message);
+    }
+
+    public static void sayBye() {
+        String bye_message = "Bye. Hope to see you again soon!\n";
+        System.out.println(bye_message);
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String user_input = sc.nextLine();
+
+            if (user_input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(user_input);
+        }
+
+        sc.close();
+    }
+}
